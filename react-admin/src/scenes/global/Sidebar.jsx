f1dcee3846d5c5dd4c4f44007c5de1de895b65f3
davique0 <davique@gmail.com>
2023-06-07 16:26:11 -0700

    topbar

--- react-admin/src/scenes/global/Sidebar.jsx
+++ react-admin/src/scenes/global/Sidebar.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { useState } from "react";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
 
 const Sidebar = () => {
   return <div>Sidebar</div>;
